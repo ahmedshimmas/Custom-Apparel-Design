93e2f3f5de4f30adf9c6c8e4f8cb0556a3548819
ahmedshimmas <mushayyy007@gmail.com>
2025-07-24 09:51:19 +0500

    updated the settings file

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,6 @@ frontend_url = os.getenv('frontend_url')
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*m!jp=bvyy&5%k1we(k%j&$y*_b(vjjh#rc94&6xj*n$&rft#-'
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
